d76ebcd92b4507ddc5023cc77d639ab280dda77f
Laura Wu <leemaiwu@gmail.com>
2023-04-22 12:14:06 -0600

    Updated header image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,10 @@ const Header = () => {
 
     return (
         <header className='top'>
-            <h1>Pokemon Team Builder!</h1>
+            <div id='logo'>
+            <img alt='Pokemon' src='https://github.com/leemaiwu/React-Pokemon-Team-Builder/blob/main/images/pokemon_title.png?raw=true' />
+            </div>
+            {/* <h1>Pokemon Team Builder!</h1> */}
         </header>
     )
 }
